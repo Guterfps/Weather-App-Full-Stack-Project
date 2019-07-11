d91c64c5119466ef8cc591429a04157c64087807
guter <ithayg@gmail.com>
2019-07-11 16:41:59 +0300

    working good

--- dist/main.js
+++ dist/main.js
@@ -1,35 +1,37 @@
 
-const tempManager=new TempManager
-const renderer=new Renderer
-const loadPage= async function(){
- await  tempManager.getDataFromDB()
- renderer.renderData(tempManager.cityData)
- console.log(tempManager.cityData)
-}
-
-const handleSearch=async function (){
-    $('.city-container').empty()
-    const input=$("#city-input").val()
-   await tempManager.getCityData(input)
+const tempManager = new TempManager
+const renderer = new Renderer
+const loadPage = async function () {
+    await tempManager.getDataFromDB()
     renderer.renderData(tempManager.cityData)
     console.log(tempManager.cityData)
 }
 
-$('body').on('click','#search',function(){
+const handleSearch = async function () {
+    $('.city-container').empty()
+    const input = $("#city-input").val()
+    await tempManager.getCityData(input)
+    renderer.renderData(tempManager.cityData)
+    console.log(tempManager.cityData)
+}
+
+$('body').on('click', '#search', function () {
     handleSearch()
 
 })
 
-$('body').on('click','.save',function(){
-    const name=$(this).siblings('h2').text()
+$('body').on('click', '.save', function () {
+    const name = $(this).siblings('h2').text()
     tempManager.saveCity(name)
     console.log(name)
 })
 
-$('body').on('click','.remove',function(){
-    const name=$(this).siblings('h2').text()
+$('body').on('click', '.remove', function () {
+
+    const name = $(this).siblings('h2').text()
     tempManager.removeCity(name)
-    
+
+    location.reload()
 })
 
 loadPage()
